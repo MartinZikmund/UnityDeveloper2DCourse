d61be87f96017b4b638c682bfb7c9701a27fa257
Martin Zikmund <martinzikmund@sphereline.com>
2018-09-25 07:18:51 +0200

    Text101

---
+++ Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]
+    private Text _textComponent;
+
+    [SerializeField]
+    private State _startingState;
+
+    private State _currentState;
+
+    // Use this for initialization
+    void Start()
+    {
+        _currentState = _startingState;
+        _textComponent.text = _currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = _currentState.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _currentState = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _currentState = nextStates[1];
+        }
+        _textComponent.text = _currentState.GetStateStory();
+    }
+}
---
+++ Text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14, 10)]
+    [SerializeField]
+    private string _storyText;
+
+    [SerializeField]
+    private State[] _nextStates;
+
+    public string GetStateStory() => _storyText;
+
+    public State[] GetNextStates() => _nextStates;
+}
