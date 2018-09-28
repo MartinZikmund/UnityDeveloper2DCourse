90ae7400c84fdac367a71ac28a9754e215f7c127
Martin Zikmund <martinzikmund@sphereline.com>
2018-09-28 06:13:59 +0200

    Paddle

---
+++ BlockBreaker/BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float screenWidthInWorldUnits = 16;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInWorldUnits);
+
+    }
+}
