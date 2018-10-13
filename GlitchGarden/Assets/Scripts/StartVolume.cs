af7549716c11fd5e38c6dfc4b9139520c9902f55
Martin Zikmund <martinzikmund@sphereline.com>
2018-10-13 21:49:33 +0200

    Finishing animations

---
+++ GlitchGarden/Assets/Scripts/StartVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartVolume : MonoBehaviour
+{
+    private MusicManager _musicManager;
+	// Use this for initialization
+	void Start ()
+	{
+	    _musicManager = FindObjectOfType<MusicManager>();
+	    if (_musicManager)
+	    {
+	        float volume = PlayerPrefsManager.MasterVolume;
+            _musicManager.SetVolume( volume );
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
