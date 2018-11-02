df73ab4e9c38be486cbe4c768030517b132c8364
Martin Zikmund <martinzikmund@sphereline.com>
2018-11-02 09:27:39 +0100

    Swiper, pin script

---
+++ BowlerMaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    public float standingThreshold = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(IsStanding());
+    }
+
+    public bool IsStanding()
+    {
+        var rotationInEuler = transform.rotation.eulerAngles;
+
+        var tiltInX = Mathf.Abs(Mathf.DeltaAngle(transform.rotation.eulerAngles.x, 0));
+        var tiltInZ = Mathf.Abs(Mathf.DeltaAngle(rotationInEuler.z, 0));
+
+        return tiltInX < standingThreshold && tiltInZ < standingThreshold;
+    }
+}
