c8046197a6dfb024ab6aa3f9228dd8250393b63f
Martin Zikmund <martinzikmund@sphereline.com>
2018-11-08 16:43:22 +0100

    Action master

---
+++ BowlerMaster/Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest
+{
+    private ActionMaster _actionMaster;
+
+    [SetUp]
+    public void Setup()
+    {
+        _actionMaster = new ActionMaster();
+    }
+
+    [Test]
+    public void OneStrikeReturnsEndTurn()
+    {
+        Assert.AreEqual(ActionMaster.Action.EndTurn, _actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void Bowl8ReturnsTidy()
+    {
+        Assert.AreEqual(ActionMaster.Action.Tidy, _actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void Bowl28ReturnsEndTurn()
+    {
+        _actionMaster.Bowl(2);
+        Assert.AreEqual(ActionMaster.Action.EndTurn, _actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void CheckResetAtStrikeInLastFrame()
+    {
+        int[] rolls = Enumerable.Repeat(1, 18).ToArray();
+        foreach (var roll in rolls)
+        {
+            _actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(ActionMaster.Action.Reset, _actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void CheckResetAtSpareInLastFrame()
+    {
+        int[] rolls = Enumerable.Repeat(1, 18).ToArray();
+        foreach (var roll in rolls)
+        {
+            _actionMaster.Bowl(roll);
+        }
+        _actionMaster.Bowl(1);
+        Assert.AreEqual(ActionMaster.Action.Reset, _actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void YouTubeRollsEndInEndGame()
+    {
+        int[] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2 };
+        foreach (var roll in rolls)
+        {
+            _actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(ActionMaster.Action.EndGame, _actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void GameEndsAtBowl20()
+    {
+        int[] rolls = Enumerable.Repeat(1, 19).ToArray();
+        foreach (var roll in rolls)
+        {
+            _actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(ActionMaster.Action.EndGame, _actionMaster.Bowl(1));
+    }
+
+    [Test]
+    public void DarylBowl20Test()
+    {
+        int[] rolls = Enumerable.Repeat(1, 18).ToArray();
+        foreach (var roll in rolls)
+        {
+            _actionMaster.Bowl(roll);
+        }
+
+        _actionMaster.Bowl(10);
+        Assert.AreEqual(ActionMaster.Action.Tidy, _actionMaster.Bowl(5));
+    }
+
+    [Test]
+    public void BenBowl20Test()
+    {
+        int[] rolls = Enumerable.Repeat(1, 18).ToArray();
+        foreach (var roll in rolls)
+        {
+            _actionMaster.Bowl(roll);
+        }
+
+        _actionMaster.Bowl(10);
+        Assert.AreEqual(ActionMaster.Action.Tidy, _actionMaster.Bowl(0));
+    }
+}
---
+++ BowlerMaster/Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActionMaster
+{
+
+    public enum Action
+    {
+        Tidy,
+        Reset,
+        EndTurn,
+        EndGame
+    }
+
+    private int[] _bowls = new int[21];
+    private int _bowlId = 1;
+
+    public Action Bowl(int pins)
+    {
+        if (pins < 0 || pins > 10) throw new ArgumentOutOfRangeException("pins");
+
+        _bowls[_bowlId - 1] = pins;
+
+        if (_bowlId == 21) return Action.EndGame;
+
+        if (_bowlId == 19 && pins == 10)
+        {
+            _bowlId++;
+            return Action.Reset;
+        }
+        else if (_bowlId == 20)
+        {
+            _bowlId++;
+            if (_bowls[18] == 10 && _bowls[19] == 0)
+            {
+                return Action.Tidy;
+            }
+            else if (_bowls[18] + _bowls[19] == 10)
+            {
+                return Action.Reset;
+            }
+            else if (_bowls[18] + _bowls[19] >= 10)
+            {  // Roll 21 awarded
+                return Action.Tidy;
+            }
+            else
+            {
+                return Action.EndGame;
+            }
+        }
+
+        if (pins == 10)
+        {
+            _bowlId += 2;
+            return Action.EndTurn;
+        }
+
+        _bowlId++;
+        if (_bowlId % 2 == 0)
+        {
+            return Action.Tidy;
+        }
+        else
+        {
+            return Action.EndTurn;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    private bool TwoStrikesLastFrame()
+    {
+        return (_bowls[19 - 1] + _bowls[20 - 1] ) % 10 == 0;
+    }
+
+    private bool Bowl21Awarded()
+    {
+        return _bowls[19 - 1] + _bowls[20 - 1] == 10;
+    }
+}
